183fe44e19e0040fe4d6db0e014ccb7f92e5c036
Barna12345 <nasrinbarna5@gmail.com>
2023-09-13 16:20:54 +0600

    added card section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+
+import './App.css'
+import Home from './componants/Home/Home'
+import './componants/Cart/Cart'
+
+function App() {
+  
+
+  return (
+    <>
+     <Home></Home>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/componants/Home/Home.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import './Home.css'
+import '../Cart/Cart';
+
+const Home = () => {
+
+    const [allActors, setAllactors] = useState([]);
+
+     useEffect(() => {
+        fetch("./data.json")
+        .then(res => res.json())
+        .then((data) => setAllactors(data))
+
+     },[]);
+    //  console.log(allActors);
+
+    return (
+        <div className='container'>
+            <div className="home-container">
+                
+                <div className="card-container">
+               {
+                allActors.map((actor) =>(
+                    <div key={actor.id} className='card'>
+                    <div className='card-img'>
+                    <img className='photo' src="https://i.ibb.co/J5bfPrZ/16926.jpg" alt="" />
+                    </div>
+                    <br />
+ 
+                    <h2>{actor.name}</h2>
+ 
+                    <p><small>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus, ab.</small></p>
+                    <div className="info">
+                        <p>Salary:{actor.salary}</p>
+ 
+                        <p>{actor.role}</p>
+                    </div>
+                    <button className='card-btn'>select</button>
+ 
+                </div>
+                ))
+               }
+
+               
+                </div>
+
+                <div className="cart">
+                    <h1>this is cart</h1>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default Home;
